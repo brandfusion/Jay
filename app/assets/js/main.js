872667c96799958e0a40c4f0bb225902cb9a9d4c
Daniel Voicu <daniel.voicu@brandfusion.ro>
2016-05-03 18:50:42 +0300

    update
    update

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
 		console.log("click");
 	});
 
+  $('.favorite-add-line').on("click", function(e){
+    e.preventDefault();
+    var productId = $(this).attr("data-product-id");
+    var quantity = $(this).parents(".favorite-quantity-wrapper").find('[name="quantity"]').val();
+    var linkAdd = "/Default.aspx?productid=@productId&amp;variantID=@productVariantId&amp;cartcmd=add&amp;quantity=@productQuantity"
+    console.log(productId);
+    console.log(quantity);
+
+  });
+
 
 
 
